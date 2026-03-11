e2ea25d83bf3ca1039a5b2c145b610d597fd852e
carolnascgoncalves <carolrsc1411@gmail.com>
2026-03-11 11:22:34 -0300

    Alteração Pagamento

--- Novvi/Novvi.Domain/Entities/Pagamento.cs
+++ Novvi/Novvi.Domain/Entities/Pagamento.cs
@@ -4,9 +4,10 @@ using Novvi.Domain.Enums;
 namespace Novvi.Domain.Entities;
 
 //1:1 -> Pedido -------
-public class Pagamento : EntidadeBase
+public class Pagamento 
 {
     //id, tipo (credito ou pix), horario
+    public Guid Id { get; private set; } = Guid.NewGuid();
     public TipoPagamento TipoPagamento { get; private set; }
     public DateTime Horario { get; private set; } = DateTime.Now;
 
